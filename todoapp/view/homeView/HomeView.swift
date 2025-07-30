084395e4a1a45917a1f64fce973747b2513af13c
umarKhan1 <muhammadomar29@outlook.com>
2025-07-30 15:21:26 +0100

    final version

---
+++ todoapp/view/homeView/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  todoapp
+//
+//  Created by Muhammad Omar on 30/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
