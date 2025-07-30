809861196347d8af189d6bd498d36490785d59d0
umarKhan1 <muhammadomar29@outlook.com>
2025-07-30 13:06:44 +0100

    first commit

---
+++ todoapp/todoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoappApp.swift
+//  todoapp
+//
+//  Created by Muhammad Omar on 30/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct todoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ todoappTests/todoappTests.swift
@@ -0,0 +1,17 @@
+//
+//  todoappTests.swift
+//  todoappTests
+//
+//  Created by Muhammad Omar on 30/07/2025.
+//
+
+import Testing
+@testable import todoapp
+
+struct todoappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
